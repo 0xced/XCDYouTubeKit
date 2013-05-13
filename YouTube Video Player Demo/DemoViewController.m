c2e8f167ff49718a0934541d9624d6af44808cfa
Cédric Luthi <cedric.luthi@gmail.com>
2013-05-14 00:45:15 +0200

    Log MPMoviePlayerPlaybackDidFinishNotification notifications

--- YouTube Video Player Demo/DemoViewController.m
+++ YouTube Video Player Demo/DemoViewController.m
@@ -17,6 +17,21 @@
 
 @implementation DemoViewController
 
+- (id) initWithNibName:(NSString *)nibName bundle:(NSBundle *)nibBundle
+{
+	if (!(self = [super initWithNibName:nibName bundle:nibBundle]))
+		return nil;
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerPlaybackDidFinish:) name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+	
+	return self;
+}
+
+- (void) dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (NSString *) title
 {
 	return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
@@ -34,6 +49,27 @@
 {
 	[self playYouTubeVideo:textField];
 	return YES;
+}
+
+#pragma mark - Notifications
+
+- (void) moviePlayerPlaybackDidFinish:(NSNotification *)notification
+{
+	MPMovieFinishReason finishReason = [notification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] integerValue];
+	NSString *reason = @"Unknown Reason";
+	switch (finishReason)
+	{
+		case MPMovieFinishReasonPlaybackEnded:
+			reason = @"Playback Ended";
+			break;
+		case MPMovieFinishReasonPlaybackError:
+			reason = @"Playback Error";
+			break;
+		case MPMovieFinishReasonUserExited:
+			reason = @"User Exited";
+			break;
+	}
+	NSLog(@"moviePlayerPlaybackDidFinish: %@", reason);
 }
 
 @end
