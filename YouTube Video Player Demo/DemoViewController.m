ef38b91bce9dcf1059f62cb6866247f609d2e808
Cédric Luthi <cedric.luthi@gmail.com>
2013-05-30 01:32:39 +0200

    Move logging from DemoViewController into AppDelegate

--- YouTube Video Player Demo/DemoViewController.m
+++ YouTube Video Player Demo/DemoViewController.m
@@ -27,9 +27,6 @@
 		return nil;
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(videoPlayerViewControllerDidReceiveMetadata:) name:XCDYouTubeVideoPlayerViewControllerDidReceiveMetadataNotification object:nil];
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerPlaybackDidFinish:) name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerPlaybackStateDidChange:) name:MPMoviePlayerPlaybackStateDidChangeNotification object:nil];
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerLoadStateDidChange:) name:MPMoviePlayerLoadStateDidChangeNotification object:nil];
 	
 	return self;
 }
@@ -101,74 +98,8 @@
 
 #pragma mark - Notifications
 
-- (void) moviePlayerPlaybackDidFinish:(NSNotification *)notification
-{
-	MPMovieFinishReason finishReason = [notification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] integerValue];
-	NSError *error = notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey];
-	NSString *reason = @"Unknown";
-	switch (finishReason)
-	{
-		case MPMovieFinishReasonPlaybackEnded:
-			reason = @"Playback Ended";
-			break;
-		case MPMovieFinishReasonPlaybackError:
-			reason = @"Playback Error";
-			break;
-		case MPMovieFinishReasonUserExited:
-			reason = @"User Exited";
-			break;
-	}
-	NSLog(@"Finish Reason: %@%@", reason, error ? [@"\n" stringByAppendingString:[error description]] : @"");
-}
-
-- (void) moviePlayerPlaybackStateDidChange:(NSNotification *)notification
-{
-	MPMoviePlayerController *moviePlayerController = notification.object;
-	NSString *playbackState = @"Unknown";
-	switch (moviePlayerController.playbackState)
-	{
-		case MPMoviePlaybackStateStopped:
-			playbackState = @"Stopped";
-			break;
-		case MPMoviePlaybackStatePlaying:
-			playbackState = @"Playing";
-			break;
-		case MPMoviePlaybackStatePaused:
-			playbackState = @"Paused";
-			break;
-		case MPMoviePlaybackStateInterrupted:
-			playbackState = @"Interrupted";
-			break;
-		case MPMoviePlaybackStateSeekingForward:
-			playbackState = @"Seeking Forward";
-			break;
-		case MPMoviePlaybackStateSeekingBackward:
-			playbackState = @"Seeking Backward";
-			break;
-	}
-	NSLog(@"Playback State: %@", playbackState);
-}
-
-- (void) moviePlayerLoadStateDidChange:(NSNotification *)notification
-{
-	MPMoviePlayerController *moviePlayerController = notification.object;
-	
-	NSMutableString *loadState = [NSMutableString new];
-	MPMovieLoadState state = moviePlayerController.loadState;
-	if (state & MPMovieLoadStatePlayable)
-		[loadState appendString:@" | Playable"];
-	if (state & MPMovieLoadStatePlaythroughOK)
-		[loadState appendString:@" | Playthrough OK"];
-	if (state & MPMovieLoadStateStalled)
-		[loadState appendString:@" | Stalled"];
-	
-	NSLog(@"Load State: %@", loadState.length > 0 ? [loadState substringFromIndex:3] : @"N/A");
-}
-
 - (void) videoPlayerViewControllerDidReceiveMetadata:(NSNotification *)notification
 {
-	NSLog(@"Metadata: %@", notification.userInfo);
-	
 	if (notification.object != self.videoPlayerViewController)
 		return;
 	
