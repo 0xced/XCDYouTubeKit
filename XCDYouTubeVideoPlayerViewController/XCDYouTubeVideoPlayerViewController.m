06405514006fc9bdae9e6ae438d8a0aeef23f73b
Cédric Luthi <cedric.luthi@gmail.com>
2013-10-04 22:54:15 +0200

    Better demo project, also iOS 7 compatible

--- XCDYouTubeVideoPlayerViewController/XCDYouTubeVideoPlayerViewController.m
+++ XCDYouTubeVideoPlayerViewController/XCDYouTubeVideoPlayerViewController.m
@@ -1,6 +1,6 @@
 //
 //  XCDYouTubeVideoPlayerViewController.m
-//  YouTube Video Player Demo
+//  XCDYouTubeVideoPlayerViewController
 //
 //  Created by Cédric Luthi on 02.05.13.
 //  Copyright (c) 2013 Cédric Luthi. All rights reserved.
---
+++ YouTube Video Player Demo/DemoFullScreenViewController.m
@@ -0,0 +1,47 @@
+//
+//  DemoFullScreenViewController.m
+//  YouTube Video Player Demo
+//
+//  Created by Cédric Luthi on 26.09.13.
+//  Copyright (c) 2013 Cédric Luthi. All rights reserved.
+//
+
+#import "DemoFullScreenViewController.h"
+
+@implementation DemoFullScreenViewController
+
+- (void) viewDidLoad
+{
+	[super viewDidLoad];
+	
+	self.videoIdentifierTextField.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"VideoIdentifier"];
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	[self.view endEditing:YES];
+}
+
+- (IBAction) play:(id)sender
+{
+	[self.view endEditing:YES];
+	
+	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:self.videoIdentifierTextField.text];
+	videoPlayerViewController.preferredVideoQualities = self.lowQualitySwitch.on ? @[ @(XCDYouTubeVideoQualitySmall240), @(XCDYouTubeVideoQualityMedium360) ] : nil;
+	[self presentMoviePlayerViewControllerAnimated:videoPlayerViewController];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+	[self play:textField];
+	return YES;
+}
+
+- (void) textFieldDidEndEditing:(UITextField *)textField
+{
+	[[NSUserDefaults standardUserDefaults] setObject:textField.text forKey:@"VideoIdentifier"];
+}
+
+@end
