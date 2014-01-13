1f0a1b99f78686e56f98a00f182fb9b032fe0e00
Cédric Luthi <cedric.luthi@gmail.com>
2014-01-13 23:23:06 +0100

    Squashed 'XCDYouTubeVideoPlayerViewController Tests/TRVSMonitor/' content from commit 088756c
    git-subtree-dir: XCDYouTubeVideoPlayerViewController Tests/TRVSMonitor
    git-subtree-split: 088756c9fc9168719032d41a2480cf193ea0149a

---
+++ TRVSMonitor/TRVSMonitor.m
@@ -0,0 +1,89 @@
+//
+//  TRVSXCTestAsync.m
+//  TRVSXCTestAsyncExample
+//
+//  Created by Travis Jeffery on 10/11/13.
+//  Copyright (c) 2013 Travis Jeffery. All rights reserved.
+//
+
+#import "TRVSMonitor.h"
+
+@interface TRVSMonitor ()
+
+@property(nonatomic) NSInteger signalsRemaining;
+@property(nonatomic) NSInteger expectedSignalCount;
+
+@end
+
+@implementation TRVSMonitor
+
++ (instancetype)monitor {
+  return [[self alloc] init];
+}
+
+- (instancetype)init {
+  return [self initWithExpectedSignalCount:1];
+}
+
+- (instancetype)initWithExpectedSignalCount:(NSInteger)expectedSignalCount {
+  self = [super init];
+  
+  if (self) {
+    self.expectedSignalCount = expectedSignalCount;
+    [self reset];
+  }
+  
+  return self;
+}
+
+- (BOOL)wait {
+  return [self waitWithTimeout:0.0];
+}
+
+- (BOOL)waitWithSignalHandler:(TRVSMonitorHandler)handler {
+  return [self waitWithTimeout:0.0 signalHandler:handler];
+}
+
+- (BOOL)waitWithTimeout:(NSTimeInterval)timeout {
+  return [self waitWithTimeout:timeout signalHandler:nil];
+}
+
+- (BOOL)waitWithTimeout:(NSTimeInterval)timeout
+          signalHandler:(TRVSMonitorHandler)handler {
+  NSDate *start = [NSDate date];
+
+  while (self.signalsRemaining > 0) {
+    [[NSRunLoop currentRunLoop]
+        runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+
+    if ([self didTimeOut:timeout fromStartDate:start]) {
+      [self reset];
+      
+      return NO;
+    }
+
+    if (handler)
+      handler(self);
+  }
+
+  [self reset];
+
+  return YES;
+}
+
+- (void)signal {
+  self.signalsRemaining -= 1;
+}
+
+#pragma mark - Private
+
+- (void)reset {
+  self.signalsRemaining = self.expectedSignalCount;
+}
+
+- (BOOL)didTimeOut:(NSTimeInterval)timeout fromStartDate:(NSDate *)startDate {
+  return (timeout > 0.0 && [[NSDate date] timeIntervalSinceDate:startDate] >
+                               timeout);
+}
+
+@end
