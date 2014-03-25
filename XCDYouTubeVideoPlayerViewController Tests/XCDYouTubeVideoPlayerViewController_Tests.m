3c4278a5162d3e3c8052869ff26ecf226f784d4a
Cédric Luthi <cedric.luthi@gmail.com>
2014-03-25 20:23:25 +0100

    Test `XCDYouTubeClient` instead of `XCDYouTubeVideoPlayerViewController`

--- XCDYouTubeVideoPlayerViewController Tests/XCDYouTubeVideoPlayerViewController_Tests.m
+++ XCDYouTubeVideoPlayerViewController Tests/XCDYouTubeVideoPlayerViewController_Tests.m
@@ -10,6 +10,8 @@
 
 #import "TRVSMonitor.h"
 #import "XCDYouTubeVideoPlayerViewController.h"
+#import "XCDYouTubeClient.h"
+#import "XCDYouTubeVideo.h"
 
 @interface XCDYouTubeVideoPlayerViewController_Tests : XCTestCase
 
@@ -45,58 +47,26 @@
 	[super tearDown];
 }
 
-- (void) testThatGangnamStyleVideoHasURL
+- (void) testThatVideoIsAvailalbeOnDetailPageEventLabel
 {
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:@"9bZkp7q19f0"];
-	[videoPlayerViewController addObserver:self forKeyPath:@"moviePlayer.contentURL" options:(NSKeyValueObservingOptions)0 context:_cmd];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		XCTFail(@"%@", notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey]);
+	[[XCDYouTubeClient new] getVideoWithIdentifier:@"9bZkp7q19f0" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(error);
+		XCTAssertNotNil(video);
 		[self.monitor signal];
 	}];
 	XCTAssertTrue([self.monitor waitWithTimeout:10]);
-	[videoPlayerViewController removeObserver:self forKeyPath:@"moviePlayer.contentURL" context:_cmd];
-	XCTAssertNotNil(videoPlayerViewController.moviePlayer.contentURL);
-}
-
-- (void) testAsynchronousVideoIdentifier
-{
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [XCDYouTubeVideoPlayerViewController new];
-	[videoPlayerViewController performSelector:@selector(setVideoIdentifier:) withObject:@"9bZkp7q19f0" afterDelay:0];
-	[videoPlayerViewController addObserver:self forKeyPath:@"moviePlayer.contentURL" options:(NSKeyValueObservingOptions)0 context:_cmd];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		XCTFail(@"%@", notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey]);
-		[self.monitor signal];
-	}];
-	XCTAssertTrue([self.monitor waitWithTimeout:10]);
-	[videoPlayerViewController removeObserver:self forKeyPath:@"moviePlayer.contentURL" context:_cmd];
-	XCTAssertNotNil(videoPlayerViewController.moviePlayer.contentURL);
-}
-
-- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-	if (context == @selector(testThatGangnamStyleVideoHasURL) || context == @selector(testAsynchronousVideoIdentifier))
-	{
-		[self.monitor signal];
-	}
-	else
-	{
-		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-	}
 }
 
 - (void) testThatGangnamStyleVideoHasMetadata
 {
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:@"9bZkp7q19f0"];
-	[[NSNotificationCenter defaultCenter] addObserverForName:XCDYouTubeVideoPlayerViewControllerDidReceiveMetadataNotification object:videoPlayerViewController queue:nil usingBlock:^(NSNotification *notification) {
-		NSDictionary *metadata = notification.userInfo;
-		XCTAssertEqualObjects(metadata[XCDMetadataKeyTitle], @"PSY - GANGNAM STYLE (\U0000ac15\U0000b0a8\U0000c2a4\U0000d0c0\U0000c77c) M/V");
-		XCTAssertTrue([metadata[XCDMetadataKeySmallThumbnailURL] isKindOfClass:[NSURL class]], @"Small thumbnail URL must be a NSURL");
-		XCTAssertTrue([metadata[XCDMetadataKeyMediumThumbnailURL] isKindOfClass:[NSURL class]], @"Medium thumbnail URL must be a NSURL");
-		XCTAssertTrue([metadata[XCDMetadataKeyLargeThumbnailURL] isKindOfClass:[NSURL class]], @"Large thumbnail URL must be a NSURL");
-		[self.monitor signal];
-	}];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		XCTFail(@"%@", notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey]);
+	[[XCDYouTubeClient new] getVideoWithIdentifier:@"9bZkp7q19f0" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(error);
+		XCTAssertEqualObjects(video.identifier, @"9bZkp7q19f0");
+		XCTAssertEqualObjects(video.title, @"PSY - GANGNAM STYLE (\U0000ac15\U0000b0a8\U0000c2a4\U0000d0c0\U0000c77c) M/V");
+		XCTAssertNotNil(video.smallThumbnailURL);
+		XCTAssertNotNil(video.mediumThumbnailURL);
+		XCTAssertNotNil(video.largeThumbnailURL);
+		XCTAssertTrue(video.streamURLs.count > 0);
 		[self.monitor signal];
 	}];
 	XCTAssertTrue([self.monitor waitWithTimeout:10]);
@@ -104,11 +74,8 @@
 
 - (void) testRestrictedVideo
 {
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:@"1kIsylLeHHU"];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		MPMovieFinishReason finishReason = [notification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] integerValue];
-		XCTAssertEqual(finishReason, MPMovieFinishReasonPlaybackError);
-		NSError *error = notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey];
+	[[XCDYouTubeClient new] getVideoWithIdentifier:@"1kIsylLeHHU" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(video);
 		XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
 		XCTAssertEqual(error.code, XCDYouTubeErrorRestrictedPlayback);
 		[self.monitor signal];
@@ -118,11 +85,8 @@
 
 - (void) testRemovedVideo
 {
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:@"BXnA9FjvLSU"];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		MPMovieFinishReason finishReason = [notification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] integerValue];
-		XCTAssertEqual(finishReason, MPMovieFinishReasonPlaybackError);
-		NSError *error = notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey];
+	[[XCDYouTubeClient new] getVideoWithIdentifier:@"BXnA9FjvLSU" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(video);
 		XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
 		XCTAssertEqual(error.code, XCDYouTubeErrorRemovedVideo);
 		[self.monitor signal];
@@ -132,11 +96,8 @@
 
 - (void) testInvalidVideoIdentifier
 {
-	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [[XCDYouTubeVideoPlayerViewController alloc] initWithVideoIdentifier:@"tooShort"];
-	[[NSNotificationCenter defaultCenter] addObserverForName:MPMoviePlayerPlaybackDidFinishNotification object:videoPlayerViewController.moviePlayer queue:nil usingBlock:^(NSNotification *notification) {
-		MPMovieFinishReason finishReason = [notification.userInfo[MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] integerValue];
-		XCTAssertEqual(finishReason, MPMovieFinishReasonPlaybackError);
-		NSError *error = notification.userInfo[XCDMoviePlayerPlaybackDidFinishErrorUserInfoKey];
+	[[XCDYouTubeClient new] getVideoWithIdentifier:@"tooShort" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(video);
 		XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
 		XCTAssertEqual(error.code, XCDYouTubeErrorInvalidVideoIdentifier);
 		[self.monitor signal];
