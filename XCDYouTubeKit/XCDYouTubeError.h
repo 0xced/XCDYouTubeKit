566a7628104f4f013c8488379344af51e5a5e304
Cédric Luthi <cedric.luthi@gmail.com>
2014-04-28 13:24:55 +0200

    Document XCDYouTubeKit error codes

--- XCDYouTubeKit/XCDYouTubeError.h
+++ XCDYouTubeKit/XCDYouTubeError.h
@@ -4,11 +4,32 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  The error domain used throughout XCDYouTubeKit.
+ */
 extern NSString *const XCDYouTubeVideoErrorDomain;
 
+/**
+ *  These values are returned as the error code property of an NSError object with the domain `XCDYouTubeVideoErrorDomain`.
+ */
 typedef NS_ENUM(NSInteger, XCDYouTubeErrorCode) {
-	XCDYouTubeErrorNetwork                = -1,  // A network error occurred.
-	XCDYouTubeErrorInvalidVideoIdentifier = 2,   // The given `videoIdentifier` string is invalid (including `nil`)
-	XCDYouTubeErrorRemovedVideo           = 100, // The video has been removed as a violation of YouTube's policy
-	XCDYouTubeErrorRestrictedPlayback     = 150  // The video is not playable because of legal reasons or the this is a private video
+	/**
+	 *  Returned when a network error occurs. See `NSUnderlyingErrorKey` in the userInfo dictionary for more information.
+	 */
+	XCDYouTubeErrorNetwork                = -1,
+	
+	/**
+	 *  Returned when the given video identifier string is invalid.
+	 */
+	XCDYouTubeErrorInvalidVideoIdentifier = 2,
+	
+	/**
+	 *  Returned when the video has been removed as a violation of YouTube's policy.
+	 */
+	XCDYouTubeErrorRemovedVideo           = 100,
+	
+	/**
+	 *  Returned when the video is not playable because of legal reasons or the this is a private video.
+	 */
+	XCDYouTubeErrorRestrictedPlayback     = 150
 };
