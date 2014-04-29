731efc8c39489c0badf68126fc19c318cb2a7403
Cédric Luthi <cedric.luthi@gmail.com>
2014-04-29 09:17:53 +0200

    Document the XCDYouTubeVideo class

--- XCDYouTubeKit/XCDYouTubeVideo.h
+++ XCDYouTubeKit/XCDYouTubeVideo.h
@@ -4,22 +4,79 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  The quality of YouTube videos. These values are used as keys in the `streamURLs` property of the `XCDYouTubeVideo` class.
+ *
+ *  The constant numbers are the YouTube [itag](https://en.wikipedia.org/wiki/YouTube#Quality_and_codecs) values.
+ */
 typedef NS_ENUM(NSUInteger, XCDYouTubeVideoQuality) {
+	/**
+	 *  Video: 240p MPEG-4 Visual | 0.175 Mbit/s
+	 *  Audio: AAC | 36 kbit/s
+	 */
 	XCDYouTubeVideoQualitySmall240  = 36,
+	
+	/**
+	 *  Video: 360p H.264 | 0.5 Mbit/s
+	 *  Audio: AAC | 96 kbit/s
+	 */
 	XCDYouTubeVideoQualityMedium360 = 18,
+	
+	/**
+	 *  Video: 720p H.264 | 2-3 Mbit/s
+	 *  Audio: AAC | 192 kbit/s
+	 */
 	XCDYouTubeVideoQualityHD720     = 22,
+	
+	/**
+	 *  Video: 1080p H.264 | 3–5.9 Mbit/s
+	 *  Audio: AAC | 192 kbit/s
+	 */
 	XCDYouTubeVideoQualityHD1080    = 37,
 };
 
+/**
+ *  Represents a YouTube video. Use the `-[XCDYouTubeClient getVideoWithIdentifier:completionHandler:]` method to obtain a `XCDYouTubeVideo` object.
+ */
 @interface XCDYouTubeVideo : NSObject <NSCopying>
 
+/**
+ *  --------------------------------
+ *  @name Accessing video properties
+ *  --------------------------------
+ */
+
+/**
+ *  The 11 characters YouTube video identifier.
+ */
 @property (nonatomic, readonly) NSString *identifier;
+/**
+ *  The title of the video.
+ */
 @property (nonatomic, readonly) NSString *title;
 
+/**
+ *  A thumbnail URL for an image of small size, i.e. 120×90. May be nil.
+ */
 @property (nonatomic, readonly) NSURL *smallThumbnailURL;
+/**
+ *  A thumbnail URL for an image of medium size, i.e. 480×360 or 640×480. May be nil.
+ */
 @property (nonatomic, readonly) NSURL *mediumThumbnailURL;
+/**
+ *  A thumbnail URL for an image of large size, i.e. 1'280×720 or 1'980×1'080. May be nil.
+ */
 @property (nonatomic, readonly) NSURL *largeThumbnailURL;
 
+/**
+ *  A dictionary of video stream URLs.
+ *
+ *  The keys are the YouTube [itag](https://en.wikipedia.org/wiki/YouTube#Quality_and_codecs) values as `NSNumber` objects. The values are the video URLs as `NSURL` objects.
+ *
+ *  You must not store the URLs for later use since they have a limited lifetime.
+ *
+ *  @see XCDYouTubeVideoQuality
+ */
 @property (nonatomic, readonly) NSDictionary *streamURLs;
 
 @end
