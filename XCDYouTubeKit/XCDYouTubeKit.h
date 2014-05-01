3d2687cf251555426844b69c2a9177b5c0b5a8c1
Cédric Luthi <cedric.luthi@gmail.com>
2014-05-01 09:22:27 +0200

    Add XCDYouTubeKit.h umbrella header

---
+++ XCDYouTubeKit/XCDYouTubeKit.h
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2013-2014 Cédric Luthi. All rights reserved.
+//
+
+#import <XCDYouTubeKit/XCDYouTubeClient.h>
+#import <XCDYouTubeKit/XCDYouTubeError.h>
+#import <XCDYouTubeKit/XCDYouTubeOperation.h>
+#import <XCDYouTubeKit/XCDYouTubeVideo.h>
+#import <XCDYouTubeKit/XCDYouTubeVideoOperation.h>
+
+#if TARGET_OS_IPHONE
+#import <XCDYouTubeKit/XCDYouTubeVideoPlayerViewController.h>
+#endif
