e3182c19526d837610a7d698c04b6efd77a34191
Cédric Luthi <cedric.luthi@gmail.com>
2014-06-23 15:50:27 +0200

    Add unit test for connection error on web page

--- XCDYouTubeKit Tests/XCDYouTubeProtectedVideosTestCase.m
+++ XCDYouTubeKit Tests/XCDYouTubeProtectedVideosTestCase.m
@@ -6,10 +6,30 @@
 
 #import <XCDYouTubeKit/XCDYouTubeClient.h>
 
+@interface NSURLRequest (Private)
++ (void) setAllowsAnyHTTPSCertificate:(BOOL)allowsAnyHTTPSCertificate forHost:(NSString *)host;
+@end
+
 @interface XCDYouTubeProtectedVideosTestCase : XCDYouTubeKitTestCase
 @end
 
 @implementation XCDYouTubeProtectedVideosTestCase
+
+- (void) setUp
+{
+	[super setUp];
+	
+	if (NSProcessInfo.processInfo.environment[@"VCR_CASSETTES_DIRECTORY"])
+		[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:@"www.youtube.com"];
+}
+
+- (void) tearDown
+{
+	[super tearDown];
+	
+	if (NSProcessInfo.processInfo.environment[@"VCR_CASSETTES_DIRECTORY"])
+		[NSURLRequest setAllowsAnyHTTPSCertificate:NO forHost:@"www.youtube.com"];
+}
 
 - (void) testProtectedVEVOVideo
 {
@@ -44,6 +64,22 @@
 		[video.streamURLs enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, NSURL *streamURL, BOOL *stop) {
 			XCTAssertTrue([streamURL.query rangeOfString:@"signature="].location != NSNotFound);
 		}];
+		[monitor signal];
+	}];
+	XCTAssertTrue([monitor waitWithTimeout:10]);
+}
+
+// With Charles
+//   * Enable SSL proxying for *.youtube.com
+//   * Tools -> Black List... -> Add host:www.youtube.com and path:watch to simulate connection error on the web page
+- (void) testProtectedVideoWithWebPageConnectionError
+{
+	TRVSMonitor *monitor = [TRVSMonitor monitor];
+	[[XCDYouTubeClient defaultClient] getVideoWithIdentifier:@"Pgum6OT_VH8" completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+		XCTAssertNil(video);
+		XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
+		XCTAssertEqual(error.code, XCDYouTubeErrorRestrictedPlayback);
+		XCTAssertEqualObjects(error.localizedDescription, @"This video contains content from WMG. It is restricted from playback on certain sites. Watch on YouTube");
 		[monitor signal];
 	}];
 	XCTAssertTrue([monitor waitWithTimeout:10]);
