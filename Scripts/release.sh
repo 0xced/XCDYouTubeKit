31ed2be75bd2629aaf89ac0351febd1da9a1a53c
Cédric Luthi <cedric.luthi@gmail.com>
2015-01-12 00:20:19 +0100

    Fix release script to handle both CocoaPods and Carthage versions

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -14,7 +14,7 @@ set -v
 sed -i "" "s/DYLIB_CURRENT_VERSION = .*;/DYLIB_CURRENT_VERSION = ${VERSION};/g" "XCDYouTubeKit.xcodeproj/project.pbxproj"
 sed -i "" "s/CURRENT_PROJECT_VERSION = .*;/CURRENT_PROJECT_VERSION = ${VERSION};/g" "XCDYouTubeKit Demo/XCDYouTubeKit Demo.xcodeproj/project.pbxproj"
 sed -i "" "s/^\(.*s.version.*=.*\)\".*\"/\1\"${VERSION}\"/" "XCDYouTubeKit.podspec"
-sed -i "" "s/\"~> .*\"/\"~> ${VERSION}\"/g" "README.md"
+sed -E -i "" "s/~> [0-9\.]+/~> ${VERSION}/g" "README.md"
 set +v
 git add "XCDYouTubeKit.xcodeproj"
 git add "XCDYouTubeKit Demo/XCDYouTubeKit Demo.xcodeproj"
