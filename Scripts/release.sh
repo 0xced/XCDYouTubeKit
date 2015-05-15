d9b6bf44e1a9f8a8db6aceba93a331ec6d29e238
Cédric Luthi <cedric.luthi@gmail.com>
2015-05-15 12:01:55 +0200

    Improve release script

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
 
+function update_badges()
+{
+    echo "Updating badges"
+    set -v
+    sed -i "" "s/$1\.svg/$2.svg/g" "README.md"
+    sed -i "" "s/branch=$1/branch=$2/g" "README.md"
+    set +v
+    git add "README.md"
+    git commit -m "Point badges to the $2 branch"
+}
+
 if [[ $# -ne 1 ]]; then
     echo "usage: $0 VERSION"
     exit 1
@@ -22,10 +33,16 @@ git add "XCDYouTubeKit.podspec"
 git add "README.md"
 git commit -m "Update version to ${VERSION}"
 
-echo "Updating badges"
-set -v
-sed -i "" "s/develop\.svg/master.svg/g" "README.md"
-sed -i "" "s/branch=develop/branch=master/g" "README.md"
-set +v
-git add "README.md"
-git commit -m "Point badges to the master branch"
+update_badges "develop" "master"
+
+git flow release finish ${VERSION}
+
+update_badges "master" "develop"
+
+echo "Things remaining to do"
+echo "  * git push with tags (master and develop)"
+echo "  * check that build is passing on travis: https://travis-ci.org/0xced/XCDYouTubeKit/"
+echo "  * pod spec lint --verbose XCDYouTubeKit.podspec"
+echo "  * pod trunk push XCDYouTubeKit.podspec --verbose"
+echo "  * create a new release on GitHub: https://github.com/0xced/XCDYouTubeKit/releases/new"
+echo "  * close milestone on GitHub if applicable: https://github.com/0xced/XCDYouTubeKit/milestones"
