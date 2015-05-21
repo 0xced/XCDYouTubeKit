ad45bbe083e4c477da92352e96d189228c19bbc7
Cédric Luthi <cedric.luthi@gmail.com>
2015-05-21 17:21:07 +0200

    Add logging capabilities
    Logs are routed through CocoaLumberjack if it is available at runtime else a basic NSLog implementation is provided with `XCDYouTubeLogger`.

---
+++ XCDYouTubeKit/XCDYouTubeLogger.m
@@ -0,0 +1,36 @@
+//
+//  Copyright (c) 2013-2015 Cédric Luthi. All rights reserved.
+//
+
+#import "XCDYouTubeLogger.h"
+
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
+
+@implementation XCDYouTubeLogger
+
++ (void) log:(BOOL)asynchronous level:(NSUInteger)level flag:(DDLogFlag)flag context:(NSInteger)context file:(const char *)file function:(const char *)function line:(NSUInteger)line tag:(id)tag format:(NSString *)format, ...
+{
+	char *logLevelString = getenv("XCDYouTubeKitLogLevel");
+	DDLogFlag logLevel = logLevelString ? strtoul(logLevelString, NULL, 0) : DDLogFlagWarning;
+	if (flag > logLevel)
+		return;
+	
+	va_list arguments;
+	va_start(arguments, format);
+	NSLog(@"[XCDYouTubeKit] %@", [[NSString alloc] initWithFormat:format arguments:arguments]);
+	va_end(arguments);
+}
+
+@end
+
+Class XCDYouTubeLogClass(void)
+{
+	static Class logClass;
+	static dispatch_once_t once;
+	dispatch_once(&once, ^{
+		logClass = NSClassFromString(@"DDLog");
+		if (![logClass methodSignatureForSelector:@selector(log:level:flag:context:file:function:line:tag:format:)])
+			logClass = [XCDYouTubeLogger class];
+	});
+	return logClass;
+}
