76c7b6d27c00b32470dc444cd1c9c88fc3b98a5a
Cédric Luthi <cedric.luthi@gmail.com>
2015-06-11 01:29:35 +0200

    Delete the YouTube v2 API code, it doesn't work at all anymore

--- XCDYouTubeKit Demo/iOS Demo/DemoAsynchronousViewController.m
+++ XCDYouTubeKit Demo/iOS Demo/DemoAsynchronousViewController.m
@@ -16,31 +16,18 @@
 - (IBAction) play:(id)sender
 {
 	NSString *apiKey = self.apiKeyTextField.text;
-	NSURL *mostPopularURL;
-	NSString *videoIdentifierKeyPath;
-	if (apiKey.length > 0)
-	{
-		// https://developers.google.com/youtube/v3/docs/videos/list
-		mostPopularURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?key=%@&chart=mostPopular&part=id", apiKey]];
-		videoIdentifierKeyPath = @"items.id";
-		[[NSUserDefaults standardUserDefaults] setObject:apiKey forKey:@"YouTubeAPIKey"];
-	}
-	else
-	{
-		// https://developers.google.com/youtube/2.0/developers_guide_protocol_video_feeds#Standard_feeds
-		mostPopularURL = [NSURL URLWithString:@"https://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json&time=today&max-results=1"];
-		videoIdentifierKeyPath = @"feed.entry.media$group.yt$videoid.$t";
-		[[NSUserDefaults standardUserDefaults] removeObjectForKey:@"YouTubeAPIKey"];
-	}
+	[[NSUserDefaults standardUserDefaults] setObject:apiKey forKey:@"YouTubeAPIKey"];
 	
 	XCDYouTubeVideoPlayerViewController *videoPlayerViewController = [XCDYouTubeVideoPlayerViewController new];
 	videoPlayerViewController.moviePlayer.backgroundPlaybackEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"PlayVideoInBackground"];
 	[self presentMoviePlayerViewControllerAnimated:videoPlayerViewController];
 	
+	// https://developers.google.com/youtube/v3/docs/videos/list
+	NSURL *mostPopularURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://www.googleapis.com/youtube/v3/videos?key=%@&chart=mostPopular&part=id", apiKey]];
 	[NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:mostPopularURL] queue:[NSOperationQueue new] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error)
 	{
 		id json = [NSJSONSerialization JSONObjectWithData:data ?: [NSData new] options:0 error:NULL];
-		NSString *videoIdentifier = [[[json valueForKeyPath:videoIdentifierKeyPath] firstObject] description];
+		NSString *videoIdentifier = [[[json valueForKeyPath:@"items.id"] firstObject] description];
 		videoPlayerViewController.videoIdentifier = videoIdentifier;
 	}];
 }
