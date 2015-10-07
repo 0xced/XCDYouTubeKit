7903821d3faeed459f6b884629da3398a5003659
Cédric Luthi <cedric.luthi@gmail.com>
2015-10-07 22:55:41 +0200

    Improve xcpretty/xcpretty-travis-formatter command construction

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -20,8 +20,7 @@ done
 
 COMMAND+=" | tee xcodebuild.log"
 
-xcpretty --version > /dev/null && COMMAND+=" | xcpretty -c"
-xcpretty-travis-formatter > /dev/null && COMMAND+=" -f `xcpretty-travis-formatter`"
+xcpretty --version > /dev/null 2>&1 && COMMAND+=" | xcpretty -c" && xcpretty-travis-formatter > /dev/null 2>&1 && COMMAND+=" -f `xcpretty-travis-formatter`"
 
 set -x
 eval "${COMMAND}" && rm xcodebuild.log
