2b2c70eabc523eb2cc900a10170adca94843dd6b
Cédric Luthi <cedric.luthi@gmail.com>
2015-11-01 00:10:00 +0100

    Add empty tvOS demo app

---
+++ XCDYouTubeKit Demo/tvOS Demo/AppDelegate.m
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2015 Cédric Luthi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+@end
---
+++ XCDYouTubeKit Demo/tvOS Demo/ViewController.m
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2015 Cédric Luthi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@end
