16c165f4ee6739e312ce64393fd7f33f1065128f
ouraigua <ouraigua@icloud.com>
2015-11-13 00:41:04 +0000

    Update XCDYouTubePlayerScript.m
    Fixed javascript extraction of the signature function following Youtube API change.

--- XCDYouTubeKit/XCDYouTubePlayerScript.m
+++ XCDYouTubeKit/XCDYouTubePlayerScript.m
@@ -15,64 +15,92 @@
 
 @implementation XCDYouTubePlayerScript
 
-- (instancetype) initWithString:(NSString *)string
-{
-	if (!(self = [super init]))
-		return nil;
-	
-	NSString *script = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-	XCDYouTubeLogTrace(@"%@", script);
-	NSRegularExpression *anonymousFunctionRegularExpression = [NSRegularExpression regularExpressionWithPattern:@"\\(function\\([^)]*\\)\\{(.*)\\}\\)\\([^)]*\\)" options:NSRegularExpressionDotMatchesLineSeparators error:NULL];
-	NSTextCheckingResult *anonymousFunctionResult = [anonymousFunctionRegularExpression firstMatchInString:script options:(NSMatchingOptions)0 range:NSMakeRange(0, script.length)];
-	if (anonymousFunctionResult.numberOfRanges > 1)
-		script = [script substringWithRange:[anonymousFunctionResult rangeAtIndex:1]];
-	else
-		XCDYouTubeLogWarning(@"Unexpected player script (no anonymous function found)");
-	
-	_context = [JSContext new];
-	_context.exceptionHandler = ^(JSContext *context, JSValue *exception) {
-		XCDYouTubeLogWarning(@"JavaScript exception: %@", exception);
-	};
-	
-	NSDictionary *environment = @{
-		@"document": @{
-			@"documentElement": @{}
-		},
-		@"location": @{
-			@"hash": @""
-		},
-		@"navigator": @{},
-	};
-	_context[@"window"] = @{};
-	for (NSString *propertyName in environment)
-	{
-		JSValue *value = [JSValue valueWithObject:environment[propertyName] inContext:_context];
-		_context[propertyName] = value;
-		_context[@"window"][propertyName] = value;
-	}
-	
-	[_context evaluateScript:script];
-	
-	NSRegularExpression *signatureRegularExpression = [NSRegularExpression regularExpressionWithPattern:@"[\"']signature[\"']\\s*,\\s*([^\\(]+)" options:NSRegularExpressionCaseInsensitive error:NULL];
-	NSTextCheckingResult *signatureResult = [signatureRegularExpression firstMatchInString:script options:(NSMatchingOptions)0 range:NSMakeRange(0, script.length)];
-	NSString *signatureFunctionName = signatureResult.numberOfRanges > 1 ? [script substringWithRange:[signatureResult rangeAtIndex:1]] : nil;
-	
-	if (signatureFunctionName)
-		_signatureFunction = self.context[signatureFunctionName];
-	
-	if (!_signatureFunction)
-		XCDYouTubeLogWarning(@"No signature function in player script");
-	
-	return self;
+- (instancetype)initWithString:(NSString *)string {
+    if (!(self = [super init]))
+        return nil;
+    
+    NSString *signatureFunctionName = [self searchRegexInString:string
+                                                    WithPattern:@".sig\\|\\|([a-zA-Z0-9]+)\\("
+                                                          range:1
+                                                 lineSeparators:YES];
+    
+    if (!signatureFunctionName) {
+        XCDYouTubeLogWarning(@"Unexpected player script (not an anonymous function)");
+    }
+    
+    _context = [JSContext new];
+    _context.exceptionHandler = ^(JSContext *context, JSValue *exception) {
+        XCDYouTubeLogWarning(@"JavaScript exception: %@", exception);
+    };
+    
+    string = [self searchRegexInString:string WithPattern:@"\\{var\\s.*;\\}" range:0 lineSeparators:YES];
+    
+    NSDictionary *environment = @{
+                                  @"navigator":@{@"presentation":@" "},
+                                  @"document":@{@"currentScript":@{@"src": @" "}},
+                                  @"location":@{@"protocol":@" ", @"hash":@" "},
+                                  };
+    for (NSString *propertyName in environment) {
+        JSValue *value = [JSValue valueWithObject:environment[propertyName] inContext:_context];
+        _context[propertyName] = value;
+    }
+    
+    [_context evaluateScript:string];
+    
+    _signatureFunction = self.context[signatureFunctionName];
+    
+    return self;
 }
 
-- (NSString *) unscrambleSignature:(NSString *)scrambledSignature
-{
+- (NSString *) unscrambleSignature:(NSString *)scrambledSignature {
 	if (!self.signatureFunction || !scrambledSignature)
 		return nil;
 	
 	JSValue *unscrambledSignature = [self.signatureFunction callWithArguments:@[ scrambledSignature ]];
 	return [unscrambledSignature isString] ? [unscrambledSignature toString] : nil;
+}
+
+- (NSString *)searchRegexInString:(NSString *)input
+                      WithPattern:(NSString *)pattern
+                            range:(NSInteger)range
+                   lineSeparators:(BOOL)lineSeparators {
+    
+    if (!input || input.length == 0) { return nil;}
+    if (!pattern || pattern.length == 0) { return nil;}
+    
+    NSString *output = nil;
+    NSArray *matches = nil;
+    NSError *error = nil;
+    
+    NSRegularExpressionOptions _options = NSRegularExpressionCaseInsensitive;
+    if (lineSeparators) {
+        _options = NSRegularExpressionCaseInsensitive | NSRegularExpressionDotMatchesLineSeparators;
+    }
+    
+    NSRegularExpression *reg = [[NSRegularExpression alloc] initWithPattern:pattern
+                                                                    options:_options
+                                                                      error:&error];
+    
+    
+    if (!error) {
+        
+        matches = [reg matchesInString:input options:0 range:NSMakeRange(0, [input length])];
+        
+        if (matches.count > 0) {
+            //NSLog(@"count: %d", matches.count);
+            NSTextCheckingResult *result = [matches objectAtIndex:0];
+            
+            if ([result numberOfRanges] >= range+1) {
+                NSRange r = [result rangeAtIndex:range];
+                if ([input substringWithRange:r]) {
+                    output = [input substringWithRange:r];
+                    return output;
+                }
+            }
+        }
+    }
+    
+    return output;
 }
 
 @end
