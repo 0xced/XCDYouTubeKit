6f99f9a21731e1351dba224d048b3a978af51d77
Cédric Luthi <cedric.luthi@gmail.com>
2017-01-31 23:47:31 +0100

    Update expired live video test

--- XCDYouTubeKit Tests/XCDYouTubeClientTestCase.m
+++ XCDYouTubeKit Tests/XCDYouTubeClientTestCase.m
@@ -87,7 +87,7 @@
 		XCTAssertNil(video);
 		XCTAssertEqualObjects(error.domain, XCDYouTubeVideoErrorDomain);
 		XCTAssertEqual(error.code, XCDYouTubeErrorRestrictedPlayback);
-		XCTAssertEqualObjects(error.localizedDescription, @"This live event has ended.");
+		XCTAssertEqualObjects(error.localizedDescription, @"This live stream recording is not available.");
 		[expectation fulfill];
 	}];
 	[self waitForExpectationsWithTimeout:5 handler:nil];
