07a8f60acfc9dea3b25062f49c83228976d3d71d
Soneé John <sonee@alphasoftware.co>
2019-01-18 12:57:11 -0400

    [Fix] JavaScript exception: ReferenceError: Can't find variable: XMLHttpRequest
    Fixes #418
    
    Thanks Josh :-)

--- XCDYouTubeKit/XCDYouTubePlayerScript.m
+++ XCDYouTubeKit/XCDYouTubePlayerScript.m
@@ -7,6 +7,7 @@
 #import <JavaScriptCore/JavaScriptCore.h>
 
 #import "XCDYouTubeLogger+Private.h"
+#import "XMLHTTPRequest.h"
 
 @interface XCDYouTubePlayerScript ()
 @property (nonatomic, strong) JSContext *context;
@@ -21,6 +22,10 @@
 		return nil; // LCOV_EXCL_LINE
 	
 	_context = [JSContext new];
+	
+	XMLHttpRequest *xmlHttpRequest = [XMLHttpRequest new];
+	[xmlHttpRequest extend:_context];
+	
 	_context.exceptionHandler = ^(JSContext *context, JSValue *exception) {
 		XCDYouTubeLogWarning(@"JavaScript exception: %@", exception);
 	};
