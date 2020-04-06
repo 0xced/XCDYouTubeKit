aadbe482216e0b5789674534721f38cf5408d2e2
Soneé John <sonee@alphasoftware.co>
2020-04-06 07:42:40 -0400

    Reindent Swift Package Manager file

--- Package.swift
+++ Package.swift
@@ -2,26 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "XCDYouTubeKit",
-    platforms: [
-        .iOS(.v8),
-        .tvOS(.v9),
-        .macOS(.v10_10)
-    ],
-    products: [
-        .library(name: "XCDYouTubeKit" , targets: ["XCDYouTubeKit"])
-    ],
-    targets: [
-        .target(
-            name: "XCDYouTubeKit",
-            path: ".",
+	name: "XCDYouTubeKit",
+	platforms: [
+		.iOS(.v8),
+		.tvOS(.v9),
+		.macOS(.v10_10)
+	],
+	products: [
+		.library(name: "XCDYouTubeKit" , targets: ["XCDYouTubeKit"])
+	],
+	targets: [
+		.target(
+			name: "XCDYouTubeKit",
+			path: ".",
 			exclude:[
 				"XCDYouTubeKit/AppledocSettings.plist",
 				"XCDYouTubeKit/Configuration.plist",
 				"XCDYouTubeKit/Info.plist",
 			],
-            sources: ["XCDYouTubeKit"],
-            publicHeadersPath: "XCDYouTubeKit"
-        )
-    ]
+			sources: ["XCDYouTubeKit"],
+			publicHeadersPath: "XCDYouTubeKit"
+		)
+	]
 )
